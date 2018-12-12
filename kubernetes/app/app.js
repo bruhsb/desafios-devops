80317c2b6c0223005b1339b7a9af10ee68edcacd
Bruno S. Brasil <bruh-sb@live.com>
2018-12-12 13:36:54 -0200

    Implementação de testes com HA

--- kubernetes/app/app.js
+++ kubernetes/app/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000;
+const fibonacci = require ('fibonacci');
 
 app.listen(port);
 console.log(`Aplicação teste executando em http://localhost: ${port}`);
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'candidato';
+  const name = process.env.NAME || 'amigo';
+  const number = process.env.NUMBER;
+  const bigNumber = fibonacci.iterate (number);
   res.send(`Olá ${name}!`);
 });
